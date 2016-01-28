3357dc43a9e967c84a208e45ebedb4519d57b272
Sabin Thomas <sabin@codiscope.com>
2016-01-28 17:06:44 -0500

    Adding session handler for express

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 app.disable('X-Powered-By');
+
+
+app.use(expressSession({
+  secret: 'keyboard cat'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
